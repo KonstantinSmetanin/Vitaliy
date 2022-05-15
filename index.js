10c4c3b0130896c252a96e3ff107aeb524c852b7
Константин <smetanin.kostya@hotmail.com>
2022-05-15 18:08:14 +0300

    Работа с записями

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const t = require('./routes/thing')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended: true}))
+app.use(t)
+
+async function start() {
+    try{
+        await mongoose.connect(
+            'mongodb+srv://Kostik:Kostik322@cluster0.7qgex.mongodb.net/findsmtn', 
+            {
+            useNewUrlParser: true,
+            }
+        )
+        .then(() => {
+            app.listen(PORT, () => {
+                console.log('Server is running...')
+            })
+        })
+    } 
+    catch (e) {
+        console.log(e)    
+    }
+}
+
+start()
---
+++ routes/thing.js
@@ -0,0 +1,59 @@
+const {Router} = require('express')
+const router = Router()
+const t = require('../models/thingModel')
+
+router.get('/', async (req, res) => {
+    const ts = await t.find({})
+
+    res.render('index', {
+        title: 'base page',
+        ts: ts.map(t => t.toJSON())
+    })
+
+    console.log(ts)
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'create things'
+    })
+})
+
+router.post('/create', async (req, res) => {
+    const ts = new t ({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        phonenum: req.body.phonenum,
+        name: req.body.name
+    })
+
+    await ts.save()
+    res.redirect('/')
+})
+
+router.post('/delete', async (req, res) => {
+    await t.deleteOne({_id: req.query.id})
+
+    res.redirect('/')
+})
+
+router.post('/update', async (req, res) => {
+    res.redirect(`/update?id=${req.query.id}`)
+})
+
+router.get('/update', async (req, res) => {
+    res.render('update', {
+        id: req.query.id
+    })
+})
+
+// router.post('/updating', async (req, res) => {
+//     t.findOneAndUpdate({_id: req.query.id}, {name: req.query.name})
+
+//     res.redirect('/')
+// })
+
+module.exports = router
